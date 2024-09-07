20321f9344f54da377d85818671633ab63f7becc
Dwi-Reygha-F <169160919+Dwi-Reygha-F@users.noreply.github.com>
2024-09-07 11:51:08 +0700

    koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,5 +1,5 @@
 <?php 
-$koneksi = mysqli_connect("localhost","root","","db_rsmadya2");
+$koneksi = mysqli_connect("localhost","root","","db_rsmadya");
  
 // Check connection
 if (mysqli_connect_errno()){
